d9312cb5a2a34b26af978f6319d90f134462de77
jensBrunel <jens.nelke@web.de>
2015-09-16 16:46:12 +0200

    Changed script

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include "dut.h"
 int main( int argc, const char* argv[] )
 {
 	printf( "\nTesting with CUnit\n\n" );
+	int a = funca(5);
 }
--- testcases/testcases.c
+++ testcases/testcases.c
@@ -1,1 +1,5 @@
 #include <stdio.h>
+#include "CUnit/Basic.h"
+
+CU_pSuite pSuite = NULL;
+
