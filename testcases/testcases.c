58581ca303711dbd7ef92f5c637dc9c2f97f9a15
jensBrunel <jens.nelke@web.de>
2015-09-17 16:00:01 +0200

    Added header and moved file

--- testcases/testcases.c
+++ testcases/testcases.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
-#include "CUnit/Basic.h"
+#include "testcases.h"
+#include "dut.h"
 
-CU_pSuite pSuite = NULL;
+void simpleTest(void) {
 
+        CU_ASSERT(2 == funca(2));
+}
+
+void setTestCases()
+{
+       CU_pSuite pSuite = NULL;
+
+	        /* initialize the CUnit test registry */
+	   if (CUE_SUCCESS != CU_initialize_registry())
+	      return CU_get_error();
+
+	   /* add a suite to the registry */
+	   pSuite = CU_add_suite("Suite_1", NULL, NULL);
+	   if (NULL == pSuite) {
+	      CU_cleanup_registry();
+	      return CU_get_error();
+	   }
+
+	   /* add the tests to the suite */
+	   if (NULL == CU_add_test(pSuite, "Simple Addition Test", simpleTest)) {
+	      CU_cleanup_registry();
+	      return CU_get_error();
+	   }
+}
---
+++ testcases/testcases.h
@@ -0,0 +1,18 @@
+/*
+ * testcases.h
+ *
+ *  Created on: 17.09.2015
+ *      Author: jens
+ */
+
+#ifndef TESTCASES_TESTCASES_H_
+#define TESTCASES_TESTCASES_H_
+
+#include "CUnit/Basic.h"
+
+extern void setTestCases();
+
+
+
+
+#endif /* TESTCASES_TESTCASES_H_ */
