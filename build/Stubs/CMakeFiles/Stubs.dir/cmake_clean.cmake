b1705cc927bfc4b88310659b0a48d7e4c57a7b10
bcsjene <jens.nelke@web.de>
2015-11-13 08:34:23 +0100

    Added Stubs dir

---
+++ build/Stubs/CMakeFiles/Stubs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Stubs.dir/Stubs.c.o"
+  "libStubs.pdb"
+  "libStubs.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/Stubs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/Stubs/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /cygdrive/d/Projekte/CUnit_Testing/Stubs
+# Build directory: /cygdrive/d/Projekte/CUnit_Testing/build/Stubs
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
